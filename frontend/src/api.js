e6057e07ff32166eb54514c73c8305c013cdcf68
V1rtualAlloc <kocovic.vladimir@gmail.com>
2024-06-08 19:11:03 +0200

    deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -7,8 +7,11 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
+// deployment url
+const apiUrl = "/my/deployment/url";
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 });
 
 api.interceptors.request.use(
@@ -22,4 +25,4 @@ api.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
-export default api
+export default api;
